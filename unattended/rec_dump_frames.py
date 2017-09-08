492b3672f8e49b41c6630ae301933f930edc1413
Pietro Saccardi <lizardm4@gmail.com>
2017-09-09 01:44:22 +0200

    Testing frame dump with multiple keyframes and motion

--- unattended/rec_dump_frames.py
+++ unattended/rec_dump_frames.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 from time import sleep
+import numpy as np
 import picamera
+import picamera.array
+from PIL import Image
 
 class MyOutput(object):
     def __init__(self, cam):
@@ -34,11 +37,23 @@ if __name__ == '__main__':
         sleep(2)
         print('Recording...')
         with MyOutput(camera) as output:
-            camera.start_recording(output, format='h264')
-            camera.wait_recording(4)
-            camera.stop_recording()
-            print('Flushed %d bytes' % output.size)
-            print('List of captured frames:')
-            for f in output.frames:
-                print('  %s' % repr(f))
-            print('Total: %d frames at %s fps.' % (len(output.frames), str(camera.framerate)))
+            with picamera.array.PiMotionArray(camera) as motion_stream:
+                camera.start_recording(output, format='h264', motion_output=motion_stream)
+                camera.wait_recording(2)
+                camera.request_key_frame()
+                camera.wait_recording(2)
+                camera.stop_recording()
+                print('Flushed %d bytes' % output.size)
+                print('List of captured frames:')
+                for f in output.frames:
+                    print('  %s' % repr(f))
+                print('Total: %d frames at %s fps.' % (len(output.frames), str(camera.framerate)))
+                for frame in range(motion_stream.array.shape[0]):
+                    data = np.sqrt(
+                        np.square(motion_stream.array[frame]['x'].astype(np.float)) +
+                        np.square(motion_stream.array[frame]['y'].astype(np.float))
+                        ).clip(0, 255).astype(np.uint8)
+                    img = Image.fromarray(data)
+                    filename = 'frame%03d.png' % frame
+                    print('Writing %s' % filename)
+                    img.save(filename)
