5f54bc674b9968bc312feb8b052fc716ed7ec523
Pietro Saccardi <lizardm4@gmail.com>
2017-09-13 20:53:59 +0200

    Updating unattended scripts and requirements.
    Now they are in sync with the mp4 encoding functionality
    provided by picamera.

--- unattended/test_mp4_output.py
+++ unattended/test_mp4_output.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-from mp4_helper import PiMP4Output
 from time import sleep
 import picamera
 
@@ -10,7 +9,7 @@ if __name__ == '__main__':
         print('Warming up camera...')
         sleep(2)
         print('Recording...')
-        with open('video.mp4', 'wb') as stream:
-            camera.start_recording(PiMP4Output(stream, camera), format='h264')
-            camera.wait_recording(2)
-            camera.stop_recording()
+        camera.start_recording('video.mp4')
+        camera.wait_recording(2)
+        camera.stop_recording()
+
