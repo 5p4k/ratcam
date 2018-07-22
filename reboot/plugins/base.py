b314ef57ee976b42ce61ddf4ad6c58c7a7d332a0
Pietro Saccardi <lizardm4@gmail.com>
2018-07-22 20:41:21 +0200

    Defined base plugin classes.

---
+++ reboot/plugins/base.py
@@ -0,0 +1,39 @@
+import Pyro4
+
+
+class PluginProcessBase:
+    @property
+    def main_process(self):
+        return self._main
+
+    @property
+    def camera_process(self):
+        return self._camera
+
+    @property
+    def telegram_process(self):
+        return self._telegram
+
+    def setup(self, main_uri, camera_uri, telegram_uri, *_, **__):
+        if self._main is not None or self._camera is not None or self._telegram is not None:
+            raise RuntimeError('Called {}.setup several times is not allowed.'.format(self.__class__.__name__))
+        self._main = Pyro4.Proxy(main_uri)
+        self._camera = Pyro4.Proxy(camera_uri)
+        self._telegram = Pyro4.Proxy(telegram_uri)
+
+    def __init__(self):
+        self._main = None
+        self._camera = None
+        self._telegram = None
+
+
+class PluginMainProcessBase(PluginProcessBase):
+    pass
+
+
+class PluginTelegramProcessBase(PluginProcessBase):
+    pass
+
+
+class PluginCameraProcessBase(PluginProcessBase):
+    pass
