e069fde9231098eb202f9c2229fca3145c63dd18
Pietro Saccardi <lizardm4@gmail.com>
2018-08-03 22:46:32 +0200

    Added imports into __init__ plugins package.

---
+++ reboot/plugins/__init__.py
@@ -0,0 +1,3 @@
+from .base import Process, ProcessPack, PluginProcessInstanceBase as ProcessBase
+from .decorators import make_plugin as make, get_all_plugins as get_all
+from .plugin_processes import PluginProcesses as ProcessesHost
