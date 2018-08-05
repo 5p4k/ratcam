86cf57bbcca9b9dd99e0a8990fb2395b1cdce693
Pietro Saccardi <lizardm4@gmail.com>
2018-08-05 23:01:49 +0200

    Implementing base for camera process.

--- reboot/plugins/__init__.py
+++ reboot/plugins/__init__.py
@@ -1,3 +1,3 @@
-from .base import Process, ProcessPack, PluginProcessInstanceBase as ProcessBase
+from .base import Process, ProcessPack, PluginProcessInstanceBase as PluginProcessBase
 from .decorators import make_plugin as make, get_all_plugins as get_all
 from .plugin_processes import PluginProcesses as ProcessesHost
--- reboot/specialized/plugin_picamera.py
+++ reboot/specialized/plugin_picamera.py
@@ -1,14 +1,19 @@
-from ..plugins.base import PluginHost, PluginBase
+from ..plugins import PluginProcessBase, make as make_plugin, Process
 from picamera import PiCamera
 from picamera.array import PiMotionAnalysis
-import Pyro4
+from Pyro4 import expose as pyro_expose
 import logging
 
 
-_log = logging.getLogger('camera_host')
+_PICAMERA_PLUGIN_NAME = 'Picamera'
+_log = logging.getLogger(_PICAMERA_PLUGIN_NAME.lower())
 
 
-class CameraPluginBase(PluginBase):
+class CameraProcessBase(PluginProcessBase):
+    @property
+    def camera_plugin(self):
+        return self.plugins[_PICAMERA_PLUGIN_NAME]
+
     def write(self, data):
         pass
 
@@ -21,14 +26,19 @@ class CameraPluginBase(PluginBase):
 
 class _CameraPluginDispatcher(PiMotionAnalysis):
     def _dispatch(self, method_name, *args, **kwargs):
-        for plugin_name, plugin_instance in self._camera_host.plugins.items():
-            method = getattr(plugin_instance.camera, 'write')
-            if method is not None:
-                try:
-                    method(*args, **kwargs)
-                except Exception as e:
-                    _log.error('Plugin %s has triggered an exception during %s: %s',
-                               plugin_name, method_name, str(e))
+        for plugin_name, plugin in self._picamera_proc.plugins.items():
+            # TODO Make sure it's ready to receive data
+            plugin_cam_process = plugin[Process.CAMERA]
+            if plugin_cam_process is None:
+                continue
+            method = getattr(plugin_cam_process, method_name)
+            if method is None:
+                continue
+            try:
+                method(*args, **kwargs)
+            except Exception as e:
+                _log.error('Plugin %s has triggered an exception during %s: %s',
+                           plugin_name, method_name, str(e))
 
     def write(self, data):
         self._dispatch('write', data)
@@ -39,20 +49,22 @@ class _CameraPluginDispatcher(PiMotionAnalysis):
     def analyze(self, array):
         self._dispatch('analyze', array)
 
-    def __init__(self, camera_host):
-        super(_CameraPluginDispatcher, self).__init__(self, camera_host.camera)
-        self._camera_host = camera_host
+    def __init__(self, picamera_proc):
+        super(_CameraPluginDispatcher, self).__init__(self, picamera_proc.camera)
+        self._picamera_proc = picamera_proc
 
 
-class CameraHost(PluginHost):
-    def __init__(self, *args, **kwargs):
-        super(CameraHost, self).__init__(*args, **kwargs)
+@make_plugin(_PICAMERA_PLUGIN_NAME, Process.CAMERA)
+class PicameraProcess(PluginProcessBase):
+    def __init__(self):
+        super(PicameraProcess, self).__init__()
         self._camera = PiCamera()
-        self._dispatcher = _CameraPluginDispatcher(self)
+        # TODO Load settings for bitrate, resolution and framerate
         self._bitrate = 750000
+        self._dispatcher = _CameraPluginDispatcher(self)
 
     def __enter__(self):
-        super(CameraHost, self).__enter__()
+        super(PicameraProcess, self).__enter__()
         _log.info('Beginning streaming data at bitrate %s, framerate %s and resolution %s.',
                   str(self.bitrate), str(self.framerate), str(self.resolution))
         self.camera.start_recording(
@@ -63,35 +75,40 @@ class CameraHost(PluginHost):
             bitrate=self.bitrate)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        super(CameraHost, self).__exit__(exc_type, exc_val, exc_tb)
+        super(PicameraProcess, self).__exit__(exc_type, exc_val, exc_tb)
         self.camera.stop_recording()
         _log.info('Stopping streaming data.')
 
-    @Pyro4.expose
     @property
     def camera(self):
         return self._camera
 
+    @pyro_expose
     @property
     def framerate(self):
         return self.camera.framerate
 
+    @pyro_expose
     @framerate.setter
     def framerate(self, value):
         self.camera.framerate = value
 
+    @pyro_expose
     @property
     def resolution(self):
         return self.camera.resolution
 
+    @pyro_expose
     @resolution.setter
     def resolution(self, value):
         self.camera.resolution = value
 
+    @pyro_expose
     @property
     def bitrate(self):
         return self._bitrate
 
+    @pyro_expose
     @bitrate.setter
     def bitrate(self, value):
         if self.camera.recording:
