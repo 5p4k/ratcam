4863f1c44bc4962b00f0dfafe200b0a2acd55fa0
Pietro Saccardi <lizardm4@gmail.com>
2018-08-28 01:25:01 +0200

    Running tests with a verbose logger.

--- tests.py
+++ tests.py
@@ -2,7 +2,10 @@ import unittest
 from misc.tests import *
 from plugins.tests import *
 from specialized.telegram_support.tests import *
+from misc.logging import ensure_logging_setup
+import logging
 
 
 if __name__ == '__main__':
+    ensure_logging_setup(logging.DEBUG, True)
     unittest.main()
