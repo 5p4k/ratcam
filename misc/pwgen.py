a900aa35cfe8ef4d7c1364a9856d104cad0aeba0
Pietro Saccardi <lizardm4@gmail.com>
2018-08-27 16:32:53 +0200

    Simplifying and using specified char set.

--- misc/pwgen.py
+++ misc/pwgen.py
@@ -1,15 +1,11 @@
 import os
 
 
-PWD_CHARS = 'abcdefghjkmnpqrstuvwxyz23456789_?!-'  # No zeros or ones or o or l
+def generate_password(length=10, charset='abcdefghjkmnpqrstuvwxyz23456789_?!-'):
+    def _pick_random_char():
+        c = len(charset)
+        while c >= len(charset):
+            c = int(os.urandom(1)[0])
+        return charset[c]
 
-
-def _pick_random_char():
-    c = len(PWD_CHARS)
-    while c >= len(PWD_CHARS):
-        c = int(os.urandom(1)[0])
-    return PWD_CHARS[c]
-
-
-def generate_password(length=10, charset=PWD_CHARS):
     return ''.join(_pick_random_char() for _ in range(length))
