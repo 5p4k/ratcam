7bf1a462fd43c8f893747b72f75b8b84b4473052
Pietro Saccardi <lizardm4@gmail.com>
2018-09-25 01:48:27 +0200

    Correctly blending sRGB space preserving gamma.

--- specialized/detector_support/ramp.py
+++ specialized/detector_support/ramp.py
@@ -1,8 +1,11 @@
+from srgb.srgb_gamma import srgb_to_linear_rgb, linear_rgb_to_srgb
+
+
 def clamp(v, min_, max_):
     return max(min(v, max_), min_)
 
 
-def blend(l, r, alpha, clamp_alpha=False):
+def linear_blend(l, r, alpha, clamp_alpha=False):
     def _dim(x):
         return len(x) if isinstance(x, (tuple, list)) else 1
     if _dim(l) != _dim(r):
@@ -12,10 +15,10 @@ def blend(l, r, alpha, clamp_alpha=False):
     if _dim(l) == 1:
         return alpha * l + (1. - alpha) * r
     else:
-        return tuple(map(lambda lr: blend(*lr, alpha, clamp_alpha=False), zip(l, r)))
+        return tuple(map(lambda lr: linear_blend(*lr, alpha, clamp_alpha=False), zip(l, r)))
 
 
-def fill_rgb_lut(val_and_color):
+def fill_linear_rgb_lut(val_and_color):
     lval = None
     lcolor = None
     for rval, rcolor in val_and_color:
@@ -24,29 +27,31 @@ def fill_rgb_lut(val_and_color):
             if n_steps <= 0:
                 continue
             for j in range(n_steps):
-                yield lval + j, tuple(map(round, blend(rcolor, lcolor, j / n_steps)))
+                yield lval + j, tuple(map(round, linear_blend(rcolor, lcolor, j / n_steps)))
         lval, lcolor = rval, rcolor
     yield lval, lcolor
 
 
-def normalize_rgb_gradient(ramp):
-    last_unit_pos = None
-    for unit_pos, color in ramp:
-        if last_unit_pos is None:
-            last_unit_pos = 0
-            if unit_pos > 0.:
+def normalize_linear_rgb_gradient(gradient):
+    last_pos = None
+    for pos, color in gradient:
+        if last_pos is None:
+            last_pos = 0
+            if pos > 0.:
                 yield 0, (0, 0, 0)
                 # And fallthough
-            elif unit_pos <= 0.:
+            elif pos <= 0.:
                 yield 0, color
                 continue
-        new_unit_pos = round(clamp(255 * unit_pos, last_unit_pos, 255))
-        if new_unit_pos != last_unit_pos:
-            yield new_unit_pos, color
-            last_unit_pos = new_unit_pos
-    if last_unit_pos < 255:
+        new_pos = round(clamp(255 * pos, last_pos, 255))
+        if new_pos != last_pos:
+            yield new_pos, color
+            last_pos = new_pos
+    if last_pos < 255:
         yield 255, (0, 0, 0)
 
 
-def make_rgb_lut(rgb_gradient_ramp):
-    yield from fill_rgb_lut(normalize_rgb_gradient(rgb_gradient_ramp))
+def make_rgb_lut(gradient):
+    yield from [(val, linear_rgb_to_srgb(color))
+                for val, color in fill_linear_rgb_lut([(v, srgb_to_linear_rgb(rgb))
+                                                       for v, rgb in normalize_linear_rgb_gradient(gradient)])]
--- specialized/detector_support/tests.py
+++ specialized/detector_support/tests.py
@@ -1,5 +1,6 @@
 import unittest
-from specialized.detector_support.ramp import normalize_rgb_gradient, make_rgb_lut, blend
+from specialized.detector_support.ramp import normalize_linear_rgb_gradient, make_rgb_lut, linear_blend
+from srgb.srgb_gamma import srgb_to_linear_rgb, linear_rgb_to_srgb
 
 
 class TestNormalizeGradient(unittest.TestCase):
@@ -12,7 +13,7 @@ class TestNormalizeGradient(unittest.TestCase):
             (0, (0, 127, 0)),
             (255, (0, 255, 0))
         ]
-        self.assertEqual(exp, list(normalize_rgb_gradient(arg)))
+        self.assertEqual(exp, list(normalize_linear_rgb_gradient(arg)))
 
     def test_with_missing_data(self):
         arg = [
@@ -24,7 +25,7 @@ class TestNormalizeGradient(unittest.TestCase):
             (26, (0, 127, 0)),
             (255, (0, 255, 0))
         ]
-        self.assertEqual(exp, list(normalize_rgb_gradient(arg)))
+        self.assertEqual(exp, list(normalize_linear_rgb_gradient(arg)))
         arg = [
             (0., (0, 127, 0)),
             (0.9, (0, 255, 0))
@@ -34,7 +35,7 @@ class TestNormalizeGradient(unittest.TestCase):
             (230, (0, 255, 0)),
             (255, (0, 0, 0)),
         ]
-        self.assertEqual(exp, list(normalize_rgb_gradient(arg)))
+        self.assertEqual(exp, list(normalize_linear_rgb_gradient(arg)))
         arg = [
             (0.1, (0, 127, 0)),
             (0.9, (0, 255, 0))
@@ -45,7 +46,7 @@ class TestNormalizeGradient(unittest.TestCase):
             (230, (0, 255, 0)),
             (255, (0, 0, 0)),
         ]
-        self.assertEqual(exp, list(normalize_rgb_gradient(arg)))
+        self.assertEqual(exp, list(normalize_linear_rgb_gradient(arg)))
 
     def test_out_of_range(self):
         arg = [
@@ -56,7 +57,7 @@ class TestNormalizeGradient(unittest.TestCase):
             (0, (0, 127, 0)),
             (255, (0, 255, 0))
         ]
-        self.assertEqual(exp, list(normalize_rgb_gradient(arg)))
+        self.assertEqual(exp, list(normalize_linear_rgb_gradient(arg)))
         arg = [
             (-0.1, (0, 127, 0)),
             (1.1, (0, 255, 0))
@@ -65,7 +66,7 @@ class TestNormalizeGradient(unittest.TestCase):
             (0, (0, 127, 0)),
             (255, (0, 255, 0))
         ]
-        self.assertEqual(exp, list(normalize_rgb_gradient(arg)))
+        self.assertEqual(exp, list(normalize_linear_rgb_gradient(arg)))
 
     def test_non_consecutive(self):
         arg = [
@@ -80,22 +81,22 @@ class TestNormalizeGradient(unittest.TestCase):
             (26, (3, 0, 0)),
             (255, (5, 0, 0))
         ]
-        self.assertEqual(exp, list(normalize_rgb_gradient(arg)))
+        self.assertEqual(exp, list(normalize_linear_rgb_gradient(arg)))
 
 
 class TestLUTHelper(unittest.TestCase):
     def test_simple(self):
         ramp = [
-            (0.0, (0,    64, 128)),
-            (0.5, (128, 192,   0)),
-            (1.0, (255,  65, 127))
+            (0.0, linear_rgb_to_srgb((0,    64, 128))),
+            (0.5, linear_rgb_to_srgb((128, 192,   0))),
+            (1.0, linear_rgb_to_srgb((255,  65, 127)))
         ]
         exp_r = list(range(127)) + list(range(127, 256))
         exp_g = list(range(64, 192)) + list(range(192, 63, -1))
         exp_b = list(range(128, 0, -1)) + list(range(128))
-        exp = list(enumerate(zip(exp_r, exp_g, exp_b)))
+        exp = list(enumerate(map(linear_rgb_to_srgb, zip(exp_r, exp_g, exp_b))))
         self.assertEqual(exp, list(make_rgb_lut(ramp)))
 
     def test_mismatch_dim_in_blend(self):
         with self.assertRaises(ValueError):
-            blend((1,2), 3, 0.5)
+            linear_blend((1, 2), 3, 0.5)
