388e5dcb0ccce738313b993aaefbcbd06d1d252a
Pietro Saccardi <lizardm4@gmail.com>
2018-10-14 23:16:37 +0200

    Added few helper parsing and printing routines for chat interaction.

---
+++ specialized/support/txtutils.py
@@ -0,0 +1,26 @@
+BOOL_YES = ['yes', 'y', 'on', '1', 't', 'true']
+BOOL_NO = ['no', 'n', 'off', '0', 'f', 'false']
+
+
+def fuzzy_bool(arg):
+    arg = arg.lower().strip()
+    if arg in BOOL_YES:
+        return True
+    elif arg in BOOL_NO:
+        return True
+    raise ValueError()
+
+
+def bool_desc(arg):
+    if arg is True:
+        return 'on'
+    elif arg is False:
+        return 'off'
+    elif arg is None:
+        return 'none'
+    raise ValueError()
+
+
+def user_desc(update):
+    user = update.message.from_user
+    return '@%s (%s %s)' % (user.username, user.first_name, user.last_name)
