fd9694da759f5de7013ff839a96f1e0429ba3717
Pietro Saccardi <lizardm4@gmail.com>
2018-11-29 22:28:43 +0100

    Allowing custom options in logging setup.

--- misc/logging.py
+++ misc/logging.py
@@ -2,13 +2,13 @@ import logging
 import re
 
 
-def ensure_logging_setup(level=logging.INFO, reset=False):
+def ensure_logging_setup(level=logging.INFO, reset=False, **kwargs):
     if reset:
         # Remove all handlers associated with the root logger object.
         # https://stackoverflow.com/a/12158233/1749822
         for handler in logging.root.handlers[:]:
             logging.root.removeHandler(handler)
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=level)
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=level, **kwargs)
 
 
 _RGX_FIRST_CAP = re.compile('(.)([A-Z][a-z]+)')
